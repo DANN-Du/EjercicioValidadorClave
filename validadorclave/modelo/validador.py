d16d53e61f02bd9acafe4da4cb25e4a9eee8b1c4
DANN-Du <danielduquegomez@gmail.com>
2024-10-16 13:44:58 -0500

    creation of the ReglaValidacionGanimede  class

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -20,5 +20,24 @@ class ReglaValidacion(ABC):
     @abstractmethod
     def es_valida(self, clave):
         pass
+class ReglaValidacionGanimedes(ReglaValidacion):
+    def __init__(self):
+        super().__init__(longitud_esperada=8)
 
+    def _contiene_caracter_especial(self, clave):
+        especiales = '@_#$%'
+        return any(char in especiales for char in clave)
+
+    def es_valida(self, clave):
+        if not self._validar_longitud(clave):
+            raise Exception("La clave debe tener una longitud de más de 8 caracteres")
+        if not self._contiene_mayuscula(clave):
+            raise Exception("La clave debe contener al menos una letra mayúscula")
+        if not self._contiene_minuscula(clave):
+            raise Exception("La clave debe contener al menos una letra minúscula")
+        if not self._contiene_numero(clave):
+            raise Exception("La clave debe contener al menos un número")
+        if not self._contiene_caracter_especial(clave):
+            raise Exception("La clave debe contener al menos un caracter especial (@, _, #, $, %)")
+        return True
 
